7611c72ac34cbab78cf47e7e8631bd9e7dbb6365
ander <andergt07@gmail.com>
2025-05-21 00:11:48 -0500

    ruta de login consumida

--- main/src/app/pages/authentication/side-login/side-login.component.ts
+++ main/src/app/pages/authentication/side-login/side-login.component.ts
@@ -1,31 +1,46 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AuthService } from '../auth.service';
 import { MaterialModule } from 'src/app/material.module';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-side-login',
+  standalone: true,
   imports: [RouterModule, MaterialModule, FormsModule, ReactiveFormsModule],
   templateUrl: './side-login.component.html',
 })
 export class AppSideLoginComponent {
 
-  constructor( private router: Router) {}
-
   form = new FormGroup({
-    uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
+
+  constructor(private router: Router, private authService: AuthService) {}
 
   get f() {
     return this.form.controls;
   }
 
   submit() {
-    // console.log(this.form.value);
-    this.router.navigate(['/']);
+    if (this.form.invalid) return;
+
+    const credentials = {
+      email: this.form.value.email as string,
+      password: this.form.value.password as string
+    };
+
+    this.authService.login(credentials).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error('Login error:', error);
+        alert('Correo o contraseña incorrectos');
+      },
+    });
   }
 }
